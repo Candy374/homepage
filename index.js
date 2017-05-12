96c5b1b18fb61a5ed54280fd9530006c0ca6fdcd
ling.huang <lingling374@qq.com>
2017-05-12 13:13:01 +0800

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by huangling on 12/5/2017.
+ */
+// var navs = [{
+//     label: '功能',
+//     src: 'http://'
+// }, {
+//     label: '我们',
+//     src: 'http://'
+// }, {
+//     label: '学堂',
+//     src: 'http://'
+// }, {
+//     label: '账号登录',
+//     src: 'http://'
+// }];
